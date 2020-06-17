d0d9778fca69ae155c96704b429213adee74fdcc
ANDROID2468 <nicholas.steyer@gmail.com>
2020-06-17 16:40:45 -0500

    V1.1: big changes adjust the timing, lower the volume during the call

--- service.sh
+++ service.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 # Made By ANDROID2468
 # This is to fix voice/SMS service at startup on some of the VS986 models
-sleep 20
+sleep 10
 #sees if your device is a VS986
 dev="$(getprop ro.product.model)"
 if [ "${dev}" = "LGVS986" ];
@@ -10,20 +10,32 @@ then
     net="$(getprop |grep gsm.network.type)"
     while [ "${net}" = "[gsm.network.type]: [Unknown]" ]
     do
+        sleep 10
+        net="$(getprop |grep gsm.network.type)"
+    done
+
+    sleep 10
+    net="$(getprop |grep gsm.network.type)"
+    while [ "${net}" = "[gsm.network.type]: [Unknown]" ]
+    do
         sleep 5
         net="$(getprop |grep gsm.network.type)"
     done
+
     sim="$(getprop |grep sim.operator.alpha)"
     # this checks if you have a verizon sim
     if [ "${sim}" = "[gsm.sim.operator.alpha]: [Verizon]" ];
     then
-        sleep 5
+        sleep 10
         # this tests your phone has made an conection 
         gsmOp="$(getprop |grep gsm.operator.alpha)"
         if [ "${gsmOp}" = "[gsm.operator.alpha]: []" ];
         then
             # This calls the testcall.com phone number 
+            service call audio 7 i32 0 i32 0 i32 8    
             am start -a android.intent.action.CALL -d tel:"804-222-1111"
+            sleep 20
+            service call audio 7 i32 0 i32 7 i32 8 
         fi
     fi
 fi
